472615de6b931ad96c4b9e9ab99f4e59406589c5
James Scott <jim.scott@urbanairship.com>
2014-05-17 17:02:57 -0700

    Updated stuffs.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject clj-porfavor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple JSON Rest interface to read only ldap."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
